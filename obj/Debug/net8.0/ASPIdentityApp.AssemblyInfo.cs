79d70579058e58cb6229db3bcdda600f013699c6
Neural S <152593439+Neural747@users.noreply.github.com>
2024-06-14 16:51:30 +0545

    DbContextClass AND User class

--- obj/Debug/net8.0/ASPIdentityApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/ASPIdentityApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ASPIdentityApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35c86b6a5424ea98c8840ca919a242127c10c055")]
 [assembly: System.Reflection.AssemblyProductAttribute("ASPIdentityApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ASPIdentityApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
